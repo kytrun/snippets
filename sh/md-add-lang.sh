399574451cc0d51da9ae57ef5aaf48adfd1209a5
Kytrun <i@kytrun.com>
2022-10-12 15:48:23 +0800

    Update md-add-lang.sh

--- sh/md-add-lang.sh
+++ sh/md-add-lang.sh
@@ -4,4 +4,4 @@
 #2. 将代码块开头 ```\n 替换为 ```lang\n
 #3. 将 ```CODE_END 替换为 ```
 cp "$1" "$1.bak.md"
-vi -c ':%s/```\n\n/```CODE_END\r\r/g | :%s/```\n/```'"$2"'\r/g | :%s/```CODE_END/```/g | :wq' $1
+vi -c ':%s/```\n\n/```CODE_END\r\r/g | :%s/```\n/```'"$2"'\r/g | :%s/```CODE_END/```/g | :wq' "$1"
