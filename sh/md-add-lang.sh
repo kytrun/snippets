c33572a6c2b4e74ea83622cfe23f8ea14db8c889
Kytrun <i@kytrun.com>
2022-10-12 15:45:43 +0800

    Update md-add-lang.sh

--- sh/md-add-lang.sh
+++ sh/md-add-lang.sh
@@ -3,5 +3,5 @@
 #1. 将代码块结尾 ```\n\n 替换为 ```CODE_END\n\n
 #2. 将代码块开头 ```\n 替换为 ```lang\n
 #3. 将 ```CODE_END 替换为 ```
-cp $1 $1.bak.md
+cp "$1" "$1.bak.md"
 vi -c ':%s/```\n\n/```CODE_END\r\r/g | :%s/```\n/```'"$2"'\r/g | :%s/```CODE_END/```/g | :wq' $1
