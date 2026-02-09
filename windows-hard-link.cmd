a3194d444b1b2d6238ed1c352220680b522d2034
Kytrun <i@kytrun.com>
2026-02-09 15:00:34 +0800

    Create windows-hard-link.cmd

---
+++ windows-hard-link.cmd
@@ -0,0 +1,5 @@
+mklink /h "D:\Program\rga-fzf-open.bat" ".\cmd-bat\rga-fzf-open.bat"
+mklink /h "D:\Program\bunx.bat" ".\cmd-bat\bunx.bat"
+mklink /h "D:\Program\git-exclude" ".\sh\git-exclude"
+
+pause
