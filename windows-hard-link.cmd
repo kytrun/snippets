92de9eef486bd5e28197ed7533cceea0407af545
Kytrun <i@kytrun.com>
2026-04-13 17:39:45 +0800

    u

--- windows-hard-link.cmd
+++ windows-hard-link.cmd
@@ -1,5 +1,6 @@
-mklink /h "D:\Program\rga-fzf-open.bat" ".\cmd-bat\rga-fzf-open.bat"
-mklink /h "D:\Program\bunx.bat" ".\cmd-bat\bunx.bat"
-mklink /h "D:\Program\git-exclude" ".\sh\git-exclude"
+mklink /h "D:\Program\portable\rga-fzf-open.bat" ".\cmd-bat\rga-fzf-open.bat"
+mklink /h "D:\Program\portable\bunx.bat" ".\cmd-bat\bunx.bat"
+mklink /h "D:\Program\portable\mvn.bat" ".\cmd-bat\mvn.bat"
+mklink /h "D:\Program\portable\git-exclude" ".\sh\git-exclude"
 
 pause
