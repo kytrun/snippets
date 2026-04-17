fcd5553000b07ce33a9bf4d064d65580f98d1cdd
Kytrun <i@kytrun.com>
2026-04-17 15:58:51 +0800

    a

--- windows-hard-link.cmd
+++ windows-hard-link.cmd
@@ -1,6 +1,7 @@
 mklink /h "D:\Program\portable\rga-fzf-open.bat" ".\cmd-bat\rga-fzf-open.bat"
 mklink /h "D:\Program\portable\bunx.bat" ".\cmd-bat\bunx.bat"
 mklink /h "D:\Program\portable\mvn.bat" ".\cmd-bat\mvn.bat"
+mklink /h "D:\Program\portable\docker.bat" ".\cmd-bat\docker.bat"
 mklink /h "D:\Program\portable\git-exclude" ".\sh\git-exclude"
 
 pause
